359c8b3e06a022f7858092df6af8fb73a5da5d3e
trungthanh123 <trungthanh248y@gmail.com>
2022-01-05 15:58:02 +0700

    ThanhNt: use axios

---
+++ components/Decks/DeckFrom.vue
@@ -0,0 +1,51 @@
+<template>
+  <!-- <div>
+    <form action="">
+      <div class="from_group">
+        <label for="">Name:</label>
+        <input
+          class="form_control"
+          type="text"
+          placeholder="Please enter name deck"
+        />
+      </div>
+      <div class="from_group">
+        <label for="">Description:</label>
+        <textarea
+          class="form_control"
+          placeholder="Please enter description"
+        ></textarea>
+      </div>
+      <div class="from_group">
+        <label for="">Thumbnail:</label>
+        <input type="text" placeholder="https://example.com/foo.png" />
+        <div class="preview"></div>
+      </div>
+      <div class="from_group d_flex justify_content_end">
+        <button class="btn btn_danger" @click.prevent="closeModal">
+          Close
+        </button>
+        <button class="btn btn_success ml_3" @click.prevent="createDeck">
+          Create
+        </button>
+      </div>
+    </form>
+  </div> -->
+  <div></div>
+</template>
+
+<script>
+export default {
+  // props: {
+  //   deck: {
+  //     type: Object,
+  //     //   require: false,
+  //     default: {
+  //       name: "",
+  //       description: "",
+  //       thumbnail: "",
+  //     },
+  //   },
+  // },
+};
+</script>
